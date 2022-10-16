69108b1f474281579b375cbb2b6741e663cb4170
Lee <leewhiz119@gmail.com>
2022-10-16 15:37:41 -0400

    made socket connection

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express = require('express') 
+var socket = require('socket.io')
 
+
+//App setup
 const app = express()
-
 const server = app.listen(2222, function(){
     console.log('server listening...')
 })
 
-
+//Static files
 app.use(express.static('public'))
+
+
+//Socket setup
+var io = socket(server)
+
+io.on('connection', function(socket){
+    console.log('made socket connection')
+})
