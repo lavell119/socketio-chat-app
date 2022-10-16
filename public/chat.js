50ea56407d7532d627b6e5e913c3a2982484b523
Lee <leewhiz119@gmail.com>
2022-10-16 19:37:06 -0400

    routed sending message to server and rendering it in the DOM

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,21 @@
 //Make connection
-var socket = io.connect('http://localhost:2222')
+const socket = io.connect('http://localhost:2222')
+
+//Query DOM
+const message = document.getElementById('message')
+const handle = document.getElementById('handle')
+const btn = document.getElementById('btn')
+const output = document.getElementById('output')
+
+//emit events
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    })
+
+//Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>'
+})
